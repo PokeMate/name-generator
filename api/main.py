f822e995a5a1edbe15c3b9e58aacdb0f6a2cd8a6
Simon <simibac93@gmail.com>
2020-03-23 09:25:03 +0100

    generator consuming model created #3

--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, escape, request, jsonify, abort
 from flask_restplus import Api, Resource, reqparse
+from generator import NameGenerator
 
 import os
 
@@ -7,13 +8,19 @@ import os
 app = Flask(__name__)
 api = Api(app, doc="/swagger/")
 
+# generator configuration
+model_path = os.path.realpath("./api/static//model.h5")
+input_names_path = os.path.realpath("./api/static/names.txt")
+generator = NameGenerator(model_path, input_names_path)
+
+
 # routes
-@api.route('/home')
+@api.route("/home")
 class Home(Resource):
-    @api.doc('home')
+    @api.doc("home")
     def get(self):
         return jsonify("Welcome to PokeMate random name generator!")
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
